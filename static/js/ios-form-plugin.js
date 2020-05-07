94e6acb76308209a2d275f90035a1e48b215d470
Ricardo <ricardo.bonis@gmail.com>
2020-05-07 04:28:22 +0200

    contact JS changes + flags + logo

--- static/js/ios-form-plugin.js
+++ static/js/ios-form-plugin.js
@@ -3,6 +3,44 @@
     this.submit(function(event) {
       event.preventDefault();
       var form = $(this);
+      var formtype = form.prop('id');
+      //this is a control point to see if the js script is getting to this point
+      console.log('form:',form);
+      console.log('formtype:', formtype);
+      var submitButton = $('input[type=submit]', form);
+      
+      $.ajax({
+        type: 'POST',
+    	      url: form.prop('action'),
+    	      accept: {
+    	        javascript: 'application/javascript'
+    	      },
+    	      data: form.serialize(),
+    	      beforeSend: function() {
+				var waitMessage = submitButton.attr("wait");
+				submitButton.prop('value', waitMessage);
+				submitButton.prop('disabled', true);
+    	      }
+      }).done(function(data) {
+      		var successMessage = submitButton.attr("success");
+      		submitButton.prop('value', successMessage);
+      		alert(submitButton.attr('success'));
+
+    	
+    			submitButton.prop('disabled', true);
+      }).fail(function(data) {
+        // Optionally alert the user of an error here...
+      });
+    });
+    return this;
+  }
+})(jQuery)
+/*
+(function($) {
+  $.fn.autosubmit = function() {
+    this.submit(function(event) {
+      event.preventDefault();
+      var form = $(this);
       //this is a control point to see if the js script is getting to this point
       console.log('form:',form);
       $.ajax({
@@ -19,3 +57,5 @@
     return this;
   }
 })(jQuery)
+
+*/
